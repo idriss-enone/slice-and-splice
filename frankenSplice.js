7293cbfd7a8bc1fea2b3efe5aff8d0402fbb2800
Idriss E <enonedjabe@gmail.com>
2025-12-24 09:23:07 +0100

    Ajout d’une implémentation alternative de frankenSplice avec boucle for commentée

--- frankenSplice.js
+++ frankenSplice.js
@@ -6,3 +6,16 @@ function frankenSplice(arr1,arr2,index){
 
     return copyArr2;
 }
+
+/**
+ * function frankenSplice(arr1,arr2,index){
+    let copyArr2 = arr2.slice();
+
+    // Insérer les éléments du premier tableau à l’index index
+    for(let i=0; i < arr1.length; ++i){
+        copyArr2.splice(index+i,0,arr1[i]);
+    }
+
+    return copyArr2;
+}
+ */
