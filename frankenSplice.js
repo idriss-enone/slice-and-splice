431185282e2119335e51cbdd2bb9fcc1abd0ccdc
Idriss E <enonedjabe@gmail.com>
2025-12-24 09:24:58 +0100

    test:ajouter des exemples d'utilisation pour frankenSplice

--- frankenSplice.js
+++ frankenSplice.js
@@ -7,6 +7,11 @@ function frankenSplice(arr1,arr2,index){
     return copyArr2;
 }
 
+console.log(frankenSplice([1, 2, 3], [4, 5],1));
+console.log(frankenSplice([1, 2], ["a", "b"], 1));
+console.log(frankenSplice(["claw", "tentacle"], ["head", "shoulders", "knees", "toes"], 2));
+console.log(frankenSplice([1, 2, 3, 4], [], 0));
+
 /**
  * function frankenSplice(arr1,arr2,index){
     let copyArr2 = arr2.slice();
